3c5c9d047de0deb273da3cccaf5c7c03f510c46f
kolpim13 <torgunakovmaks2@gmail.com>
2022-06-10 02:48:20 +0300

    Upd. 10.06.12
    1. Map creation rebuild - Now creates 3 files: low, high resolution maps and descriptor with parameters
    
    2. Changed localization file.
    For now can only use low resolution map to find place
    
    3. Rebuild all launch files

--- scripts/create_map.py
+++ scripts/create_map.py
@@ -1,125 +1,145 @@
 #!/usr/bin/env python
 import rospy
 import sys
+import pathlib
 import json
-import traceback
 import math
+
 from gazebo_msgs.srv import GetModelState
 from gazebo_msgs.msg import ModelState
 from sensor_msgs.msg import LaserScan
+#=======================================================================================#
 
-# Variables
-pub = None
-robot_state = None
-PATH_TO_FILE = "/home/maks/catkin_ws/src/diplom/records/map2.json"
-model_to_move = None
-laser_topic_to_read = None
-
-# Functions
 def laser_topic_read():
     laser_topic_name = "/laser/scan"
     laser = rospy.wait_for_message(laser_topic_name, LaserScan)
-    distances = laser.ranges
-    range_min = laser.range_min
-    range_max = laser.range_max
-    return (distances, range_min, range_max)
+    distances = [round(item, 5) for item in laser.ranges]
+    return distances
 
-def compress_laser_data(data: list, range_min: float, range_max: float, limits: int):
-    length = len(data)
-    ran = range_max / limits
-    out = [0 for i in range(limits)]
-
-    for item in data:
-        item = 0 if item == float('-inf') else item
-        item = range_max if item == float('inf') else item
-        item = item - 0.001 if item == range_max else item
-        lim = math.floor(item / ran)
-        out[lim] = out[lim] + 1
-
-    return out
-
-def robot_move_on(x: float, y: float):
-    global pub, robot_state
+def model_move_on(x: float, y: float):
+    global pub_model_state, model_state
 
     # Position
-    robot_state.pose.position.x = x
-    robot_state.pose.position.y = y
+    model_state.pose.position.x = x
+    model_state.pose.position.y = y
 
     # Publish
-    pub.publish(robot_state)
+    pub_model_state.publish(model_state)
 
-def append_to_file(file_path, pos, distances, compressed):
-    with open(PATH_TO_FILE, 'a') as jsonfile:
-        json.dump({"pos": pos, "hist": compressed}, jsonfile)
+def append_to_file(file_path, pos, distances):
+    with open(file_path, 'a') as jsonfile:
+        json.dump({"pos": pos, "raw_data": distances}, jsonfile)
         jsonfile.write(str('\n'))
 
 def get_real_robot_pos():
-    robot_pose_topic_name = ""
     gms = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-    robot_pose = gms("lidar", "")
+    robot_pose = gms(gazebo_model_name, "")
     real_x = robot_pose.pose.position.x
     real_y = robot_pose.pose.position.y
     return (real_x, real_y)
 
-if __name__ == '__main__':
-    rospy.init_node('map_creater')
+def generate_descriptor(path_to_desc):
+    global pos_x_begin, pos_y_begin, pos_x_end, pos_y_end, lowRes_step, highRes_step 
+    global lowRes_lines, highRes_lines
 
-    #model_to_move = rospy.get_param("model_to_move", "liadr")
-    #laser_topic_to_read = rospy.get_param("laser_topic_to_read", "/laser/scan")
+    obj = {
+        "x_begin": str(pos_x_begin),
+        "y_begin": str(pos_y_begin),
+        "x_end": str(pos_x_end),
+        "y_end": str(pos_y_end),
+        "lowRes": str(lowRes_step),
+        "highRes": str(highRes_step),
+        "lowRes_i": str(lowRes_lines),
+        "highRes_i": str(highRes_lines),
+    }
 
-    rate = rospy.Rate(0.01)
+    with open(path_to_desc, 'w+') as jsonfile:
+        json.dump(obj, jsonfile)
 
-    robot_state = ModelState()
-    pub = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size = 1)
-    
-    # Robot state adjust
-    robot_state.model_name = 'lidar'
-    robot_state.reference_frame = 'willowgarage'
-    #robot_state.reference_frame = 'cafe'
-    robot_state.pose.position.z = 0.05
-    robot_state.pose.orientation.w = 1
-    robot_state.pose.orientation.x = 0
-    robot_state.pose.orientation.y = 0
-    robot_state.pose.orientation.z = 0
+def generate_map(path_to_map, step) -> int:
+    global pos_x_begin, pos_y_begin, pos_x_end, pos_y_end, lowRes_step, highRes_step, laser_range_max
 
-    # First and last map positions
-    x_i = 0.0
-    y_i = 0.0
-    x_l = 55.1
-    y_l = 45.1
-    x_step = 0.1    # in meters
-    y_step = 0.1    # in meters
-
-    # Create or clear file were we are writing
-    with open(PATH_TO_FILE, 'w+') as jsonfile:
+    with open(path_to_map, 'w+') as jsonfile:
         pass
 
-    # Main algorithm
-    x = x_i
-    y = y_i
-    i = 0
-    while y <= y_l:
-        while x <= x_l:
-            robot_move_on(x, y)
-            rospy.sleep(0.1)
-            (distances, range_min, range_max) = laser_topic_read()
-            compressed = compress_laser_data(distances, range_min, range_max, 60)
+    x = pos_x_begin
+    y = pos_y_begin
+    i: int = 0
+
+    while y <= pos_y_end:
+        while x <= pos_x_end:
+            model_move_on(x, y)
+            rate.sleep()
+
             (real_x, real_y) = get_real_robot_pos()
-            append_to_file(PATH_TO_FILE, (round(real_x, 3), round(real_y, 3), 0.0), distances, compressed)
+            distances = laser_topic_read()
+            for item in distances:
+                item = 0.0 if item == float('-inf') else item
+                item = laser_range_max if item == float('inf') else item
+                item = item - 0.001 if item == laser_range_max else item
+            append_to_file(path_to_map, (round(real_x, 1), round(real_y, 1), 0.0), distances)
+
             i += 1
-            if i == 10:
+            if i % 10 == 0:
                 rospy.loginfo("x = %f, y = %f done", x, y)
-                i = 0
-            x = x + x_step
-        x = x_i
-        y = y + y_step
+            x += step
 
-        rate.sleep()
+        x = pos_x_begin
+        y += step
+    return i
 
+def generate_map_void():
+    global PATH_TO_MAP_LOW_RES
 
-    #while not rospy.is_shutdown():
-    #    x += 0.1
-    #    y += 0.1
-    #    # Position
-    #    robot_state.pose.position.x = x
-    #    robot_state.pose.position.y = y
+    with open(PATH_TO_MAP_LOW_RES, 'w+') as jsonfile:
+        for i in range(10):
+            model_move_on(0, 0)
+            rate.sleep()
+            (real_x, real_y) = get_real_robot_pos()
+#=======================================================================================#
+
+if __name__ == '__main__':
+    # ROS 
+    rospy.init_node("map_creater")
+    rate = rospy.Rate(10)
+
+    # Parameters read
+    map_name = rospy.get_param("~map_name")
+    gazebo_model_name = rospy.get_param("~gazebo_model_name")
+    reference_frame = rospy.get_param("~reference_frame")
+    pos_x_begin = rospy.get_param("~pos_x_begin")
+    pos_y_begin = rospy.get_param("~pos_y_begin")
+    pos_x_end = rospy.get_param("~pos_x_end")
+    pos_y_end = rospy.get_param("~pos_y_end")
+    lowRes_step = rospy.get_param("~lowRes_step")
+    highRes_step = rospy.get_param("~highRes_step")
+    laser_range_min = rospy.get_param("~laser_min")
+    laser_range_max = rospy.get_param("~laser_max")
+
+    BASE_DIR = pathlib.Path(__file__).resolve(strict=True).parent
+    PATH_TO_MAP_FOLDER = pathlib.Path(BASE_DIR.parent, "records")
+    PATH_TO_MAP_DESCRIPTOR = pathlib.Path(PATH_TO_MAP_FOLDER, map_name + "_description.txt")
+    PATH_TO_MAP_LOW_RES = pathlib.Path(PATH_TO_MAP_FOLDER, map_name + "_lowRes.txt")
+    PATH_TO_MAP_HIGH_RES = pathlib.Path(PATH_TO_MAP_FOLDER, map_name + "_highRes.txt")
+
+    # Publishers and subscribers
+    model_state = ModelState()
+    pub_model_state = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size = 1)
+
+    # Adjusting
+    model_state.model_name = gazebo_model_name
+    model_state.reference_frame = reference_frame
+    model_state.pose.position.z = 0.1
+    model_state.pose.orientation.w = 1
+    model_state.pose.orientation.x = 0
+    model_state.pose.orientation.y = 0
+    model_state.pose.orientation.z = 0
+
+    # Generate two maps
+    generate_map_void()
+    lowRes_lines = generate_map(PATH_TO_MAP_LOW_RES, lowRes_step)
+    highRes_lines = generate_map(PATH_TO_MAP_HIGH_RES, highRes_step)
+
+    # Create desriptor for maps
+    generate_descriptor(PATH_TO_MAP_DESCRIPTOR)
+#=======================================================================================#
--- scripts/localization.py
+++ scripts/localization.py
@@ -3,74 +3,219 @@ import rospy
 import sys
 import json
 import math
+import time
+import pathlib
+import numpy as np
+
+from gazebo_msgs.srv import GetModelState
 from sensor_msgs.msg import LaserScan
+#=======================================================================================#
 
-# Variables
-PATH_TO_FILE = "/home/maks/catkin_ws/src/diplom/records/map2.json"
-
-# Functions
+# AUXILIARY FUNCTIONS
 def laser_topic_read():
     laser_topic_name = "/laser/scan"
     laser = rospy.wait_for_message(laser_topic_name, LaserScan)
-    distances = laser.ranges
-    range_min = laser.range_min
-    range_max = laser.range_max
-    return (distances, range_min, range_max)
+    #distances = [round(item, 5) for item in laser.ranges]
+    distances = np.array(laser.ranges)
+    return distances
 
-def compress_laser_data(data: list, range_min: float, range_max: float, limits: int):
-    length = len(data)
-    ran = range_max / limits
-    out = [0 for i in range(limits)]
+def find_neighbors(pos, step_x, step_y):
+    top = (pos[0], round(pos[1] - step_y, 1), pos[2])
+    right = (round(pos[0] + step_x, 1), pos[1], pos[2])
+    bot = (pos[0], round(pos[1] + step_y, 1), pos[2])
+    left = (round(pos[0] - step_x, 1), pos[1], pos[2])
+
+    top_right = (round(pos[0] + step_x, 1), round(pos[1] - step_y, 1), pos[2])
+    bot_right = (round(pos[0] + step_x, 1), round(pos[1] + step_y, 1), pos[2])
+    bot_left = (round(pos[0] - step_x, 1), round(pos[1] + step_y, 1), pos[2])
+    top_left = (round(pos[0] - step_x, 1), round(pos[1] - step_y, 1), pos[2])
+
+    return (top, right, bot, left, top_right, bot_right, bot_left, top_left)
+#=======================================================================================#
+
+# COMPRESSION RAW DATA TO HISTOGRAMS
+def compress_pos_absolute(data):
+    global range_min, range_max, hist_pos_limits
+
+    range = range_max / hist_pos_limits
+    hist = np.zeros(hist_pos_limits)
 
     for item in data:
-        item = 0 if item == float('-inf') else item
-        item = range_max if item == float('inf') else item
+        item = 0.0 if item == float('-inf') else item
+        item = float(range_max) if item == float('inf') else item
         item = item - 0.001 if item == range_max else item
-        lim = math.floor(item / ran)
-        out[lim] = out[lim] + 1
+        lim = math.floor(item / range)
+        hist[lim] += 1 
 
-    return out
+    return hist
 
-def compare_histograms_absoluteValue(hist1: list, hist2: list):
-    sum = 0.0
+def compress_orient_minInSector(data):
+    global range_min, range_max, laser_limits, hist_orient_limits
+
+    range = range_max / hist_orient_limits
+    hist = np.zeros(hist_orient_limits)
+
+    ranges_in_sector = laser_limits / hist_orient_limits
+    fr: int = 0
+    to: int = 0
+    for i in range(hist_orient_limits):
+        to = round(ranges_in_sector * i)
+        hist[i] = min(data[fr:to])
+        fr = to
+
+    return hist
+
+# MATCH TWO HISTOGRAMS
+def match_pos_absolute(hist1, hist2):
+    global laser_limits
+    amount = laser_limits * 2
+
+    sum = 0
     for i in range(len(hist1)):
         sum += abs(hist2[i] - hist1[i])
 
-    return sum
+    error = sum / amount
+    return error
+#=======================================================================================#
+
+# Returns index of the best element in the arrays
+def find_pos_lowRes(hist):
+    global map_pos_low, map_loc_low, LR_i
+
+    min_error: float = 100.0
+    best_index: int = 0
+    best_pos = None
+
+    for i in range(LR_i):
+        error = match_pos_absolute(hist, map_pos_low[i])
+        if min_error > error:
+            min_error = error
+            best_index = i
+
+    return best_index
+#=======================================================================================#
+
+# PRE PROCESSING
+def read_descriptor():
+    global PATH_TO_MAP_DESCRIPTOR
+
+    with open(PATH_TO_MAP_DESCRIPTOR, 'r') as jsonfile:
+        data = json.load(jsonfile)
+        x_begin = float(data['x_begin'])
+        y_begin = float(data['y_begin'])
+        x_end = float(data['x_end'])
+        y_end = float(data['y_end'])
+        LR = float(data['lowRes'])
+        HR = float(data['highRes'])
+        LR_i = int(data['lowRes_i'])
+        HR_i = int(data['highRes_i'])
+
+    return (x_begin, y_begin, x_end, y_end, LR, HR, LR_i, HR_i)
+
+def buffer_map(path_to_map, total_lines):
+    global x_begin, y_begin, x_end, y_end
+    global hist_pos_limits, hist_orient_limits, func_compress_pos 
+
+    map_pos = np.zeros((total_lines, hist_pos_limits), dtype=np.int32)
+    map_orient = np.zeros((total_lines, hist_orient_limits), dtype=np.float32)
+    map_loc = np.zeros((total_lines, 3), dtype=np.float32)
+
+    with open(path_to_map, 'r') as jsonfile:
+        for index in range(int(total_lines)):
+            line = jsonfile.readline()
+            json_line = json.loads(line)
+            raw_data = json_line["raw_data"]
+            map_pos[index, :] = func_compress_pos(raw_data)
+            # map_orient[index, :] = 
+            map_loc[index, :] = np.array(json_line["pos"], dtype=np.float32)
+
+    return (map_pos, map_orient, map_loc)
+#=======================================================================================#
 
 if __name__ == '__main__':
-    rospy.init_node('localization')
-    rate = rospy.Rate(1)
+    # ROS 
+    rospy.init_node("map_localization")
+    rate = rospy.Rate(0.5)
 
+    # Ros parameters read
+    rospy.loginfo("Read all parameters")
+    map_name = rospy.get_param("~map_name")
+    gazebo_model_name = rospy.get_param("~gazebo_model_name")
+    hist_pos_limits = rospy.get_param("~hist_pos_limits")
+    hist_orient_limits = rospy.get_param("~hist_orient_limits")
+    range_min = rospy.get_param("~laser_min")
+    range_max = rospy.get_param("~laser_max")
+    laser_limits = rospy.get_param("~laser_amount")
+    threshold = rospy.get_param("~threshold")
+    func_compress_pos = rospy.get_param("~func_compress_pos")           # Way for data to be compressed
+    func_compress_orient = rospy.get_param("~func_compress_orient")
+    func_match_pos = rospy.get_param("~func_match_pos")                 # Way for histograms to be matched
+    func_match_orient = rospy.get_param("~func_match_orient")
+
+    # Set relatives path 
+    BASE_DIR = pathlib.Path(__file__).resolve(strict=True).parent
+    PATH_TO_MAP_FOLDER = pathlib.Path(BASE_DIR.parent, "records")
+    PATH_TO_MAP_DESCRIPTOR = pathlib.Path(PATH_TO_MAP_FOLDER, map_name + "_description.txt")
+    PATH_TO_MAP_LOW_RES = pathlib.Path(PATH_TO_MAP_FOLDER, map_name + "_lowRes.txt")
+    PATH_TO_MAP_HIGH_RES = pathlib.Path(PATH_TO_MAP_FOLDER, map_name + "_highRes.txt")
+
+    # Read parameters from file
+    (x_begin, y_begin, x_end, y_end, LR, HR, LR_i, HR_i) = read_descriptor()
+
+    # Choose compression and matching functions
+        # pos compression function
+    if func_compress_pos == "absolute":
+        func_compress_pos = compress_pos_absolute
+    if func_compress_orient == "minInSector":
+        func_compress_orient = compress_orient_minInSector
+    #if func_match_pos == "absolute":
+    #    func_match_pos = 
+    #if func_match_orient == "absolute":
+    #    func_match_orient = 
+
+    # Buffer maps
+    rospy.loginfo("Buffer maps from files start")
+    (map_pos_low, map_orient_low, map_loc_low) = buffer_map(PATH_TO_MAP_LOW_RES, LR_i)
+    (map_pos_high, map_orient_high, map_loc_high) = buffer_map(PATH_TO_MAP_HIGH_RES, HR_i)
+    rospy.loginfo("Buffer maps from files done:")
+    rospy.loginfo(map_pos_low)
+    rospy.loginfo(map_orient_low)
+    rospy.loginfo(map_loc_low)
+    rospy.loginfo("================================================================")
+
+    # Main loop
     while not rospy.is_shutdown():
-        #aver_range = [0 for i in range(1000)]
-        #for i in range(20):
-        #    (distances, range_min, range_max) = laser_topic_read()
-        #    aver_range = [aver_range[j] + distances[j] for j in range(1000)]
-        #    rospy.sleep(0.2)
-        #aver_range = [aver_range[j] / 20 for j in range(1000)]
-        #histogram = compress_laser_data(aver_range, range_min, range_max, 60)
+        start_time = time.time()
 
-        (distances, range_min, range_max) = laser_topic_read()
-        histogram = compress_laser_data(distances, range_min, range_max, 60)
+        # Read laser raw_data and convert them into histogram
+        ranges = laser_topic_read()
+        hist = func_compress_pos(ranges)
+        rospy.loginfo("Raw data was read and compressed into histograms")
 
-        #rospy.loginfo(histogram)
-        #rospy.loginfo(distances)
+        # Find best candidate on the low resolution map
+        best_index_low = find_pos_lowRes(hist)
+        best_low = np.array(map_loc_low[best_index_low])
+        rospy.loginfo("Best candidate on the low resolution map")
+        rospy.loginfo("x_low = %.5f, y_high = %.5f", best_low[0], best_low[1])
 
-        pos = None
-        prob_estimation = 1.7976931348623157e+308
 
-        jsonfile = open(PATH_TO_FILE, 'r')
-        all_lines = jsonfile.readlines()
-        jsonfile.close()
+        #rospy.loginfo("Nearest positions to check on the high resolution map")
+        #rospy.loginfo("Bla Bla")
 
-        for line in all_lines:
-            json_line = json.loads(line)
-            hist_i = json_line['hist']
-            estim = compare_histograms_absoluteValue(histogram, hist_i)
-            if estim < prob_estimation:
-                prob_estimation = estim
-                pos = json_line['pos']
 
-        rospy.loginfo("Current position: x = %.2f, y = %.2f, phi = %.2f", pos[0], pos[1], pos[2])
+        #rospy.loginfo("Best candidate on the high resolution map")
+        #rospy.loginfo("x_high = %.5f, y_high = %.5f", best_high[0], best_high[1])
+
+
+        #rospy.loginfo("Approximated position")
+        #rospy.loginfo("x_approx = %.5f, y_approx = %.5f", approx[0], approx[1])
+
+
+        #rospy.loginfo("Orientation")
+        #rospy.loginfo("phi = %.5f", orient)
+
+
+        rospy.loginfo("This step took %.3f", (time.time() - start_time))
+        rospy.loginfo("================================================================")
         rate.sleep()
+#=======================================================================================#
